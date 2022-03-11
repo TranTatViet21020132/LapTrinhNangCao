e31e14b9875443c985c29515ee726b3cac30fe26
TranTatViet21020132 <100561242+TranTatViet21020132@users.noreply.github.com>
2022-03-12 00:02:58 +0700

    first commit

--- Week4/Bai3.cpp
+++ Week4/Bai3.cpp
@@ -1,1 +1,53 @@
-
+void printArrow(int n, bool left)
+{
+    if(left == false)
+    {
+        for(int i=0; i <n; i++)
+        {
+            int k = i*2;
+            while(k--)
+            cout << " ";
+            for(int j=n-i;j >=1;j --)
+            {
+                cout <<'*';
+            }
+            cout << '\n';
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            int k= i*2;
+            while(k--)
+            {
+                cout << ' ';
+            }
+            for(int j =1; j <=n-i;j++)
+            {
+                cout <<'*';
+            }
+            cout << '\n';
+        }
+    }
+    else
+    {
+        for(int i=n; i >0; i--)
+        {
+            int k = i-1;
+            while(k--)
+            cout << " ";
+            for(int j=i;j >=1;j --)
+            {
+                cout <<'*';
+            }
+            cout << '\n';
+        }
+        for(int i=1;i<n;i++)
+        {
+            int k=i;
+            while(k--)
+                cout <<' ';
+            for(int j=0;j<=i;j++)
+                cout<<'*';
+            cout << '\n';
+        }
+    }
+}
